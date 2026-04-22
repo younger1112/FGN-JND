6dc60b1c9fa00c69d968a4d0de4b3c159c0295e3
younger1112 <130195815+younger1112@users.noreply.github.com>
2026-04-22 21:34:08 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,10 @@
 import numpy as np
 from PIL import Image
-
-
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg",".bmp"])
-
-
 def load_img(filepath):
     img = Image.open(filepath).convert('RGB')
-    #img = img.resize((640, 480), Image.BICUBIC)
     return img
-
-
 def save_img(image_tensor, filename):
     image_numpy = image_tensor.float().numpy()
     image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
